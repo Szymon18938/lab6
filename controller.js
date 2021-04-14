bd2e34f4a9b513317c5c04b7c2f7a4ecefec80f6
Szymon18938 <61495762+Szymon18938@users.noreply.github.com>
2021-04-14 12:29:31 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -4,29 +4,29 @@ portfolioApp.controller('GalleryListCtrl', function($scope) {
     { 'title':'Rzym',
     'sekcja':'Wycieczka',
      'when':'2011-02-21',
-     'thumbnailUrl':'/1.jpg'
+     'thumbnailUrl':'1.jpg'
     },
     { 'title':'Bruksela',
      'when':'2014-08-04',
-     'thumbnailUrl':'/2.jpg'
+     'thumbnailUrl':'2.jpg'
      
     },
     { 'title':'Warszawa',
      'when':'2012-08-04',
-     'thumbnailUrl':'/3.jpg'
+     'thumbnailUrl':'3.jpg'
     },
     { 'title':'Krym',
      'when':'2011-05-04',
-     'thumbnailUrl':'/4.jpg',
+     'thumbnailUrl':'4.jpg',
      'sekcja':'Wycieczka'
     },
     { 'title':'Londyn',
      'when':'2013-03-04',
-     'thumbnailUrl':'/5.jpg'
+     'thumbnailUrl':'5.jpg'
     },
     { 'title':'Moskwa',
      'when':'2017-08-21',
-     'thumbnailUrl':'/6.jpg'
+     'thumbnailUrl':'6.jpg'
     }
     ];
     }); 
